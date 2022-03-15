433304a220fbea95f9ff799933da5173feff17e8
hack <hugolgsuarez@gmail.com>
2022-03-15 09:58:27 +0100

    valor comparador dataset

--- js/volteo cartas.js
+++ js/volteo cartas.js
@@ -1,15 +1,25 @@
 "use strict";
 const cards = document.querySelectorAll(".card");
-
 const reveal = (e) => {
   const currentCard = e.currentTarget;
   currentCard.classList.add("flipped");
-
-  setTimeout(() => {
-    currentCard.classList.remove("flipped");
-  }, 1000);
+  for (const card of cards) {
+    card.addEventListener("click", (e2) => {
+      const currentCard2 = e2.currentTarget;
+      currentCard2.classList.add("flipped");
+      if (currentCard.dataset === currentCard2.dataset) {
+        console.log("hola");
+      } else {
+        setTimeout(() => {
+          currentCard.classList.remove("flipped");
+          currentCard2.classList.remove("flipped");
+        }, 1000);
+      }
+    });
+  }
 };
 
+console.dir(cards[1]);
 for (const card of cards) {
   card.addEventListener("click", reveal);
 }
