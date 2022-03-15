3066cc1fc20f1897c1917ef4f56bf0fbaeb94c91
hack <hugolgsuarez@gmail.com>
2022-03-15 21:30:31 +0100

    varios

--- js/volteocartas e comparacion.js
+++ js/volteocartas e comparacion.js
@@ -1,25 +1,38 @@
 "use strict";
-const cards = document.querySelectorAll(".card");
+
+let textConter = new Array();
+console.log(textConter);
+
 const reveal = (e) => {
   const currentCard = e.currentTarget;
   currentCard.classList.add("flipped");
-  for (const card of cards) {
-    card.addEventListener("click", (e2) => {
-      const currentCard2 = e2.currentTarget;
-      currentCard2.classList.add("flipped");
-      if (currentCard.dataset === currentCard2.dataset) {
-        console.log("hola");
-      } else {
-        setTimeout(() => {
-          currentCard.classList.remove("flipped");
-          currentCard2.classList.remove("flipped");
-        }, 1000);
-      }
-    });
+  textConter.unshift(currentCard);
+  if (textConter[0].textContent === textConter[1].textContent) {
+    console.log("ola");
+    delete textConter[0];
+    delete textConter[1];
+  } else if (textConter[0].textContent !== textConter[1].textContent) {
+    setTimeout(() => {
+      textConter[0].classList.remove("flipped");
+      textConter[1].classList.remove("flipped");
+      delete textConter[0];
+      delete textConter[1];
+    }, 2000);
   }
 };
 
-console.dir(cards[1]);
+const cards = document.querySelectorAll(".card");
+
 for (const card of cards) {
   card.addEventListener("click", reveal);
 }
+
+// const cards = document.querySelectorAll(".card");
+
+// const reveal = (event) => {
+//   const currentCard = event.currentTarget;
+//   currentCard.classList.add("flipped");
+
+// for (const card of cards) {
+//   card.addEventListener("click", reveal);
+// }
